5080d35fc6b1b5c86a9794f507f0c3b8ce1182a0
Vanessa Ramos <Vanessa Ramos@DESKTOP-9BEC718>
2017-05-29 11:42:06 -0300

    Classes de teste

---
+++ src/main/java/br/edu/ifpe/AgendaVirtual/test/UsuarioTest.java
@@ -0,0 +1,14 @@
+package br.edu.ifpe.AgendaVirtual.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsuarioTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
