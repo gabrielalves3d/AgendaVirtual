d7a4fc5ca831169724637522709495fe2330edef
vanessaramos <vanessaalramos@hotmail.com>
2017-08-22 15:03:23 -0300

    na

---
+++ src/main/java/br/edu/ifpe/AgendaVirtual/builders/BuilderAgendamento.java
@@ -0,0 +1,67 @@
+package br.edu.ifpe.AgendaVirtual.builders;
+
+import java.util.Date;
+
+import org.hibernate.dialect.DB2390Dialect;
+
+import br.edu.ifpe.AgendaVirtual.model.entity.Agendamento;
+import br.edu.ifpe.AgendaVirtual.model.entity.Disciplina;
+
+public class BuilderAgendamento {
+	
+	private int id;
+	private String atividade;
+	private Date dataHora;
+	private String descricao;
+	private Disciplina nome;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAtividade() {
+		return atividade;
+	}
+	public void setAtividade(String atividade) {
+		this.atividade = atividade;
+	}
+	public Date getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(Date dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Disciplina getNome() {
+		return nome;
+	}
+	public void setNome(Disciplina nome) {
+		this.nome = nome;
+	}
+	
+	public BuilderAgendamento(){
+	}
+	
+	public BuilderAgendamento(int id, String atividade, Date dataHora, String descricao, Disciplina nome){
+		this.id = id;
+		this.atividade = atividade;
+		this.dataHora = dataHora;
+		this.descricao = descricao;
+		this.nome = nome;
+	}
+	
+	public Agendamento construirAgendamento(){
+		return new Agendamento(id, atividade, dataHora, descricao, nome); 
+	}
+	
+
+}
---
+++ src/main/java/br/edu/ifpe/AgendaVirtual/controller/AgendamentoController.java
@@ -0,0 +1,63 @@
+package br.edu.ifpe.AgendaVirtual.controller;
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import br.edu.ifpe.AgendaVirtual.model.entity.Agendamento;
+import br.edu.ifpe.AgendaVirtual.persistencia.generico.RepositorioGenerico;
+import br.edu.ifpe.AgendaVirtual.persistencia.implementacoes.FabricaRepositorio;
+
+
+@ManagedBean
+@SessionScoped
+public class AgendamentoController {
+	
+	RepositorioGenerico<Agendamento, Integer> repositorioAgendamento = null;
+	
+	private Agendamento selecionar;
+	
+	public Agendamento getSelecionar(){
+		return selecionar;
+	}
+	
+	public void setSelecionar(Agendamento selecionar){
+		this.selecionar = selecionar;
+	}
+	
+	public AgendamentoController(){
+		this.repositorioAgendamento = FabricaRepositorio.fabricarRepositorio(
+				FabricaRepositorio.Agendamento,FabricaRepositorio.BD);
+	}
+	public String inserir(Agendamento agendamento){
+		this.repositorioAgendamento.inserir(agendamento);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("O Agendamento foi inserido!"));
+		
+		return "ApresentarAgendamento.xhtml";
+		
+	}
+	
+	public String alterar(Agendamento agendamento){
+		this.repositorioAgendamento.alterar(agendamento);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("O Agendamento foi alterado!"));
+		
+		return "ApresentarAgendamento.xhtml";
+	}
+	
+	public Agendamento recuperarAgendamento(int id){
+		return this.repositorioAgendamento.recuperar(id);
+		
+	}
+	
+	public void excluir(Agendamento agendamento){
+		this.repositorioAgendamento.excluir(agendamento);
+		
+	}
+	
+	public List<Agendamento> recuperarTodosAgendamento(){
+		return this.repositorioAgendamento.recuperarTodos();
+	}
+
+}
