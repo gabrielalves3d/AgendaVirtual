24ad1d57b1f02b9fda72923d36926d5fd21d05ad
vanessaramos <vanessaalramos@hotmail.com>
2017-10-06 21:08:56 -0300

    Teste dos métodos getters da classe Agendamento

--- src/main/java/br/edu/ifpe/av/test/AgendamentoTest.java
+++ src/main/java/br/edu/ifpe/av/test/AgendamentoTest.java
@@ -2,13 +2,40 @@ package br.edu.ifpe.av.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import org.junit.Assert;
 import org.junit.Test;
 
-public class AgendamentoTest {
+import br.edu.ifpe.av.model.entity.Agendamento;
+import br.edu.ifpe.av.model.entity.Disciplina;
+import br.edu.ifpe.av.model.entity.Usuario;
 
+public class AgendamentoTest {
+	
+        Disciplina d = new Disciplina();
+        Usuario usr = new Usuario();
+        Date dataHora = new Date("18/02/1999");
+        
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getAtividadeTest() {
+		Agendamento ag = new Agendamento(1,"Trabalho",dataHora,"csdcder", d , usr);
+	    String resultado = ag.getAtividade();
+	    Assert.assertEquals("Trabalho", resultado);
 	}
+	@Test
+	public void getDataTest() {
+		Agendamento ag = new Agendamento(1,"Trabalho",dataHora,"csdcder", d , usr);
+	    Date resultado = ag.getDataHora();
+	    Assert.assertEquals(dataHora, resultado);
+	}
+	@Test
+	public void getDescricaoTest() {
+		Agendamento ag = new Agendamento(1,"Trabalho",dataHora,"Atividade avaliativa", d , usr);
+	    String resultado = ag.getDescricao();
+	    Assert.assertEquals("Atividade avaliativa", resultado);
+	}
+	
 }
 	
--- src/main/java/br/edu/ifpe/av/test/RepositorioDisciplinaTest.java
+++ src/main/java/br/edu/ifpe/av/test/RepositorioDisciplinaTest.java
@@ -8,17 +8,18 @@ import org.mockito.mock.*;
 import br.edu.ifpe.av.model.entity.DaoManagerHiber;
 import br.edu.ifpe.av.model.entity.Disciplina;
 import br.edu.ifpe.av.persistencia.generico.RepositorioGenerico;
+import br.edu.ifpe.av.persistencia.implementacoes.RepositorioDisciplina;
 import junit.framework.Assert;
 
 public class RepositorioDisciplinaTest {
 	
 	DaoManagerHiber dao = DaoManagerHiber.getInstance();
+	RepositorioGenerico<Disciplina, Integer> repositorio;
 	
-	@Test
-	public void testInserir(Disciplina u) {
-		
-			dao.persist(u);
-		}
+	public void inserirDisciplinaTest(Disciplina t) {
+		 
+	
+	}
 	
 	}
 		 
