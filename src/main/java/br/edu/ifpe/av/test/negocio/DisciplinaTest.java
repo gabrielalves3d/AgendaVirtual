4445c69fad3e7e3698580503f4995b6c7e18f483
MayaraSilvaRafa <mayarasilva.mrs@gmail.com>
2017-10-08 08:39:49 -0300

    Adicionando testes na classe Disciplina

--- src/main/java/br/edu/ifpe/av/test/negocio/DisciplinaTest.java
+++ src/main/java/br/edu/ifpe/av/test/negocio/DisciplinaTest.java
@@ -2,13 +2,28 @@ package br.edu.ifpe.av.test.negocio;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import br.edu.ifpe.av.model.entity.Disciplina;
+import junit.framework.Assert;
 
 public class DisciplinaTest {
 
+	List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setNomeDisciplinaTest() {
+		Disciplina disciplina = new Disciplina();
+		disciplina.setNome("Mat");
+		Assert.assertEquals("Mat", disciplina.getNome());
+		
+		
+		
+		
+
 	}
 
 }
